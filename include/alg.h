f4ac45cff5cb9c3e408c8a29ab1bc319ba264927
anvar <mr.nazyrov1997@mail.ru>
2026-06-05 15:30:11 +0300

    new st 0

--- include/alg.h
+++ include/alg.h
@@ -1,7 +1,10 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_ALG_H_
-#define INCLUDE_ALG_H_
+// Copyright 2024 Nazyrov A.A.
+
+#ifndef ALG_H
+#define ALG_H
+
+#include <cstdint>
 
 uint64_t factorial(uint8_t n);
 
-#endif  // INCLUDE_ALG_H_
+#endif
--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,11 @@
-// Copyright 2026 NNTU-CS
+// Copyright 2024 Nazyrov A.A.
+
 #include "alg.h"
 
 uint64_t factorial(uint8_t n) {
-    uint64_t res = 1;
-    for (uint8_t i = 1; i <= n; ++i) {
-        res = res * i;
+    uint64_t result = 1;
+    for (uint8_t i = 2; i <= n; ++i) {
+        result *= i;
     }
-    return res;
+    return result;
 }
