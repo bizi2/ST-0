e9df275b141dcdf8de65bec8c9b53fd84609c33f
anvar <mr.nazyrov1997@mail.ru>
2026-06-05 15:23:42 +0300

    Implement factorial

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,10 @@
 // Copyright 2026 NNTU-CS
-#include <cstdint>
+#include "alg.h"
 
 uint64_t factorial(uint8_t n) {
-  return 1;
+    uint64_t res = 1;
+    for (uint8_t i = 1; i <= n; ++i) {
+        res = res * i;
+    }
+    return res;
 }
